9114a0c3f00e00f46a384d638863081104fc259f
Saji <sahajking20@gmail.com>
2025-06-10 13:15:03 -0700

    Update v5.50.lua

--- Scanner_Source/v5.50.lua
+++ Scanner_Source/v5.50.lua
@@ -34,9 +34,9 @@ local timer = 0; -- // times how long it takes to load the script
 local backdoorfound = false; -- // this will turn to true or false depending on if vuln found
 local vulnremote = nil; -- // if a remote with a vuln or backdoor is found it will be referenced in this variable
 
-local safetime = 0.35; -- // lower will cause faster scan times but it will mess up more and have false positives
--- // higher numbers (like 0.35 which is the default) will take longer but be a good scanner
--- // 0.35 is the best for all situations and prob wont need to be changed
+local safetime = 0.25; -- // lower will cause faster scan times but it will mess up more and have false positives
+-- // higher numbers (like 0.25 which is the default) will take longer but be a good scanner
+-- // 0.25 is the best for all situations and prob wont need to be changed
 
 local scanninghint = Instance.new("Hint", workspace); -- // creates a hint to track scanner progress for the skids
 scanninghint.Text = "STRAWBERRY V5: Scanning Game. Be patient. (Check F9 menu for progress) (Game might freeze for a bit)";
@@ -71,17 +71,18 @@ local function remoteBackdoored(remote)
 	print("STRAWBERRY V5: "..remote.Name.." /isbackdoored: "..tostring(isDestroyed(testpart)).." / "..remote:GetFullName());
 	if isDestroyed(testpart) then
 		vulnremote = remote;
+		return true;
 	end;
-	return isDestroyed(testpart);
+	return false;
 end; -- // checks a remote event for a backdoor or vulnerability by firing it and seeing if it does something
 
 local function scan()
 	if backdoorfound then return end;
-	for i, v in pairs(game:GetDescendants()) do
+	for i, v in ipairs(game:GetDescendants()) do
 		if v:IsA("RemoteEvent") then
 			if not v.Parent then continue end;
 			if v.Parent.Name == "RobloxReplicatedStorage" then continue end;
-			if remoteBackdoored(v) then
+			if remoteBackdoored(v) == true then
 				backdoorfound = true;
 				print("found1!!?!?!");
 				return; -- // backdoor found so breaks the loop

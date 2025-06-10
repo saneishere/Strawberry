0153b1ffa3a531df42176c3c85d9b3f9b290e134
Saji <sahajking20@gmail.com>
2025-06-10 13:16:39 -0700

    Update Hook_Source.lua

--- Hook_Source.lua
+++ Hook_Source.lua
@@ -1,3 +1,6 @@
+-- // this is the the obfuscated file in this repo aka Hook.lua but unobfuscated without our webhook
+-- // reason that one is obfuscated it because it contains our webhook. but heres the code, just replace the webhook with yours.
+
 function SendMessage(url, message)
     local http = game:GetService("HttpService")
     local headers = {

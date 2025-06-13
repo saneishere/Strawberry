ba0fb85b62e14ef71664553ba8b1658ae26f0c74
saxo <129598973+saneishere@users.noreply.github.com>
2025-06-13 02:33:18 +0200

    Update Current_Version.lua

--- Current_Version.lua
+++ Current_Version.lua
@@ -1,25 +1,3 @@
---[[
-⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⡄⠀⠀⠀⠀⢀⠀⠀
-⠀⠀⠀⠀⠀⠀⣏⠓⠒⠤⣰⠋⠹⡄⠀⣠⠞⣿⠀⠀
-⠀⠀⠀⢀⠄⠂⠙⢦⡀⠐⠨⣆⠁⣷⣮⠖⠋⠉⠁⠀
-⠀⠀⡰⠁⠀⠮⠇⠀⣩⠶⠒⠾⣿⡯⡋⠩⡓⢦⣀⡀
-⠀⡰⢰⡹⠀⠀⠲⣾⣁⣀⣤⠞⢧⡈⢊⢲⠶⠶⠛⠁
-⢀⠃⠀⠀⠀⣌⡅⠀⢀⡀⠀⠀⣈⠻⠦⣤⣿⡀⠀⠀
-⠸⣎⠇⠀⠀⡠⡄⠀⠷⠎⠀⠐⡶⠁⠀⠀⣟⡇⠀⠀
-⡇⠀⡠⣄⠀⠷⠃⠀⠀⡤⠄⠀⠀⣔⡰⠀⢩⠇⠀⠀
-⡇⠀⠻⠋⠀⢀⠤⠀⠈⠛⠁⠀⢀⠉⠁⣠⠏⠀⠀⠀
-⣷⢰⢢⠀⠀⠘⠚⠀⢰⣂⠆⠰⢥⡡⠞⠁⠀⠀⠀⠀
-⠸⣎⠋⢠⢢⠀⢠⢀⠀⠀⣠⠴⠋⠀⠀⠀⠀⠀⠀⠀
-⠀⠘⠷⣬⣅⣀⣬⡷⠖⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠈⠁⠀
-
-made by C:\Drive and Saji⠀⠀⠀
-
-C:\Drive - Gui, commands
-Saji - Commands, scanner
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-]]--
-
---=======================================================================================================================--
-loadstring(game:HttpGet("https://raw.githubusercontent.com/C-Dr1ve/Strawberry/refs/heads/main/Scanner_Source/V6.lua"))()
---=======================================================================================================================--
+-- Strawberry V7 - The only tool you'll need.
+-- Loader written by sane. If it breaks, it's your fault.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/C-Dr1ve/Strawberry/main/Scanner_Source/V7.lua"))()
